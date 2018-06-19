2526547fd894829fc8a9e3a286d237848515ca42
Toshinori Tsugita <tsugita.dev@gmail.com>
2018-06-19 03:07:50 +0300

    Don't need to use geo restriction

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -47,8 +47,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   restrictions {
     geo_restriction {
-      restriction_type = "whitelist"
-      locations        = ["US", "CA", "GB", "DE", "JP"]
+      restriction_type = "none"
     }
   }
 
@@ -66,9 +65,9 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   viewer_certificate {
     cloudfront_default_certificate = true
-    acm_certificate_arn      = "${var.acm_certificate_arn}"
-    ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1"
+    acm_certificate_arn            = "${var.acm_certificate_arn}"
+    ssl_support_method             = "sni-only"
+    minimum_protocol_version       = "TLSv1"
   }
 }
 
