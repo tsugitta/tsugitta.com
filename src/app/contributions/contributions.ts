dccc6ac87faf3512a6e67f039a13f0441676c522
Toshinori Tsugita <tsugita.dev@gmail.com>
2023-01-14 15:07:17 +0900

    Update contributions

--- src/app/contributions/contributions.ts
+++ src/app/contributions/contributions.ts
@@ -29,7 +29,7 @@ export const contributions: Contribution[] = [
   },
   {
     title: 'Throw error with Stripe error code key in Android',
-    type: 'Issue',
+    type: 'ClosedIssue',
     date: '2017-10-06',
     url: 'https://github.com/tipsi/tipsi-stripe/issues/130',
   },
@@ -47,9 +47,65 @@ export const contributions: Contribution[] = [
     url: 'https://github.com/mizchi/hard-reducer/issues/2',
   },
   {
+    title: 'Add size property to ActionButonItemProperties interface',
+    type: 'MergedPR',
+    date: '2018-06-12',
+    url: 'https://github.com/mastermoo/react-native-action-button/pull/283',
+  },
+  {
     title: 'Allow Selector to accept arguments besides state',
     type: 'ClosedIssue',
     date: '2018-06-25',
     url: 'https://github.com/ngrx/platform/issues/1152',
+  },
+  {
+    title:
+      'BottomSheetScrollView cannot scroll when used with Dynamic Snap Point',
+    type: 'ClosedIssue',
+    date: '2021-06-20',
+    url: 'https://github.com/gorhom/react-native-bottom-sheet/issues/496',
+  },
+  {
+    title: "Can't use LessThan for Union field",
+    type: 'Issue',
+    date: '2022-06-27',
+    url: 'https://github.com/typeorm/typeorm/issues/9152',
+  },
+  {
+    title: '[RFC]: validate accepts a function as same as validateWith',
+    type: 'ClosedIssue',
+    date: '2022-10-28',
+    url: 'https://github.com/redwoodjs/redwood/issues/6759',
+  },
+  {
+    title: 'feat: add CustomValidator',
+    type: 'MergedPR',
+    date: '2022-12-08',
+    url: 'https://github.com/redwoodjs/redwood/pull/7051',
+  },
+  {
+    title: 'fix: allow FindOptionsWhere to accept LessThan with Union',
+    type: 'PR',
+    date: '2022-12-08',
+    url: 'https://github.com/typeorm/typeorm/pull/9607',
+  },
+  {
+    title: 'fix: correct Cell prop types when used with beforeQuery',
+    type: 'PR',
+    date: '2022-12-09',
+    url: 'https://github.com/redwoodjs/redwood/pull/7063',
+  },
+  {
+    title: 'feat: allow to re-patch DataSource & Repository',
+    type: 'PR',
+    date: '2022-12-26',
+    url: 'https://github.com/Aliheym/typeorm-transactional/pull/23',
+  },
+  {
+    title:
+      '@Transactional({ connectionName }) does not switch repositories injected by NestJS',
+    type: 'Issue',
+    date: '2023-01-04',
+    url: 'https://github.com/Aliheym/typeorm-transactional/issues/24',
   },
 ];
